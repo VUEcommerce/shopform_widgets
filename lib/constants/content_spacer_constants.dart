599d90ea9b963b0ab2d2b7399f2b6d359bf79cee
cuong <cuong@shopform.sg>
2021-04-26 18:15:09 +0700

    wip

---
+++ lib/constants/content_spacer_constants.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ContentSpacerConstants {
+  static const horizontalFour = SizedBox(
+    width: 4.0,
+  );
+  static const horizontalEight = SizedBox(
+    width: 8.0,
+  );
+  static const horizontalSixteen = SizedBox(
+    width: 16.0,
+  );
+  static const verticalTwo = SizedBox(
+    height: 2.0,
+  );
+  static const verticalFour = SizedBox(
+    height: 4.0,
+  );
+  static const verticalEight = SizedBox(
+    height: 8.0,
+  );
+  static const verticalSixteen = SizedBox(
+    height: 16.0,
+  );
+  static const verticalTwelve = SizedBox(
+    height: 12.0,
+  );
+  static const verticalTwenty = SizedBox(
+    height: 20.0,
+  );
+  static const verticalTwentyFour = SizedBox(
+    height: 24.0,
+  );
+  static const verticalThirtySix = SizedBox(
+    height: 36.0,
+  );
+  static const verticalFortyFour = SizedBox(
+    height: 44.0,
+  );
+  static const verticalFiftySix = SizedBox(
+    height: 56.0,
+  );
+  static const verticalSixty = SizedBox(
+    height: 60.0,
+  );
+}
