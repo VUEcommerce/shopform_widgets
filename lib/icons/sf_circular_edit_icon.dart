007d57de2852ce667dbd837eab3622406ec3b821
cuong <cuong@shopform.sg>
2021-04-26 17:07:14 +0700

    wip

---
+++ lib/icons/sf_circular_edit_icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopform_widgets/constants/icon_name_constants.dart';
+
+class SFCircularEditIcon extends StatelessWidget {
+  final double height;
+  final double width;
+
+  const SFCircularEditIcon({
+    this.height = 22,
+    this.width = 22,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      iconCircularEdit,
+      semanticsLabel: iconCircularEditSemantics,
+      height: height,
+      width: width,
+    );
+  }
+}
