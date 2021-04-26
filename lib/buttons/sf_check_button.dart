703874c6b1ea2814d691aaf46ba3e65f92e86216
cuong <cuongnguyen@b13technology.com>
2021-04-26 16:36:07 +0700

    wip

---
+++ lib/buttons/sf_check_button.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:shopform_widgets/ultils/sf_app_color.dart';
+
+enum SFCheckButtonState { checked, unchecked, halfChecked }
+
+class SFCheckButton extends StatelessWidget {
+  final VoidCallback onTap;
+  SFCheckButtonState checkButtonState;
+  final String tag;
+  final double paddingWidth;
+  final double paddingHeight;
+  final double iconSize;
+  final bool isChecked;
+  final Color backgroundColor;
+  final Border border;
+
+  SFCheckButton({
+    this.paddingWidth = 18,
+    this.paddingHeight = 18,
+    this.onTap,
+    this.tag,
+    this.iconSize = 20,
+    this.checkButtonState,
+    this.isChecked = false,
+    this.backgroundColor,
+    this.border,
+  }) {
+    if (isChecked != null && checkButtonState == null) {
+      if (isChecked) {
+        checkButtonState = SFCheckButtonState.checked;
+      } else {
+        checkButtonState = SFCheckButtonState.unchecked;
+      }
+    }
+  }
+
+  bool get _isCheckedOrHalfChecked =>
+      checkButtonState == SFCheckButtonState.checked ||
+      checkButtonState == SFCheckButtonState.halfChecked;
+
+  bool get _isHalfChecked => checkButtonState == SFCheckButtonState.halfChecked;
+
+  bool get _isChecked => checkButtonState == SFCheckButtonState.checked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: iconSize + paddingWidth,
+      height: iconSize + paddingHeight,
+      child: Material(
+        color: backgroundColor ?? SFAppColor.transparent,
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            color: SFAppColor.transparent,
+            alignment: Alignment.center,
+            child: Container(
+              width: iconSize,
+              height: iconSize,
+              decoration: BoxDecoration(
+                color: _isCheckedOrHalfChecked
+                    ? SFAppColor.tulipTreeGold
+                    : SFAppColor.transparent,
+                borderRadius: BorderRadius.circular(2),
+                border: _getBorder(),
+              ),
+              child: _isHalfChecked
+                  ? Icon(
+                      Icons.remove,
+                      key: Key('check_box_half_checked_$tag'),
+                      size: iconSize,
+                      color: SFAppColor.white,
+                    )
+                  : _isChecked
+                      ? Icon(
+                          Icons.check,
+                          key: Key('check_box_checked_$tag'),
+                          size: iconSize,
+                          color: SFAppColor.white,
+                        )
+                      : Container(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Border _getBorder() {
+    if (_isCheckedOrHalfChecked) {
+      return null;
+    }
+    return border ??
+        Border.all(
+          color: SFAppColor.tulipTreeGold,
+          width: 1,
+        );
+  }
+}
---
+++ lib/buttons/sf_elevated_button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:shopform_widgets/ultils/sf_app_color.dart';
+
+class SFElevatedButton extends StatelessWidget {
+  final double height;
+  final String text;
+  final TextStyle style;
+  final double horizontalPadding;
+  final BorderRadius borderRadius;
+  final VoidCallback onTap;
+  final bool isExpanded;
+  final Color backgroundColor;
+  final Color strokeColor;
+
+  SFElevatedButton({
+    this.height = 46,
+    @required this.text,
+    @required this.style,
+    this.horizontalPadding = 8,
+    @required this.onTap,
+    this.isExpanded = false,
+    this.borderRadius = const BorderRadius.all(Radius.circular(6)),
+    this.backgroundColor = SFAppColor.tulipTreeGold,
+    this.strokeColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return isExpanded ? buildContent() : IntrinsicWidth(child: buildContent());
+  }
+
+  bool get isStroke => strokeColor != null;
+
+  Widget buildContent() {
+    return Material(
+      color: Colors.transparent,
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          height: height,
+          padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+          decoration: BoxDecoration(
+            border: isStroke ? Border.all(color: strokeColor) : null,
+            borderRadius: borderRadius,
+            color: backgroundColor,
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: style,
+          ),
+        ),
+      ),
+    );
+  }
+}
