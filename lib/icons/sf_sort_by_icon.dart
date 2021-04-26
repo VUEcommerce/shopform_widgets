100c4d82f9f48ae1d49f13f1bf0e00b44f91592e
cuong <cuong@shopform.sg>
2021-04-26 18:17:52 +0700

    wip

---
+++ lib/icons/sf_sort_by_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:shopform_widgets/constants/icon_name_constants.dart';
+
+class SFSortByIcon extends StatelessWidget {
+  final double height;
+  final double width;
+
+  const SFSortByIcon({this.height, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      iconSortBy,
+      semanticsLabel: iconSortBySemantics,
+      height: height ?? 20.0,
+      width: width ?? 20.0,
+    );
+  }
+}
