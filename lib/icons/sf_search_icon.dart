ae39a67be6d38a6dd5fcd88ee0d13b942a3ee3f9
cuong <cuong@shopform.sg>
2021-04-29 16:09:48 +0700

    wip

---
+++ lib/icons/sf_search_icon.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopform_widgets/utils/sf_app_color.dart';
+
+import 'package:shopform_widgets/constants/icon_name_constants.dart';
+
+class SFSearchIcon extends StatelessWidget {
+  final double height;
+  final double width;
+  final Color color;
+
+  const SFSearchIcon({
+    this.height = 24.0,
+    this.width = 24.0,
+    this.color = SFAppColor.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      iconSearch,
+      semanticsLabel: iconSearchSemantics,
+      height: height,
+      width: width,
+      color: color,
+    );
+  }
+}
