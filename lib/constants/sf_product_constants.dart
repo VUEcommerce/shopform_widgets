621b9dd65c55adf7a23621828c2e7ec08e3e2bd9
cuong <cuong@shopform.sg>
2021-04-29 18:33:43 +0700

    wip

--- lib/constants/sf_product_constants.dart
+++ lib/constants/sf_product_constants.dart
@@ -1,6 +1,7 @@
 class SFProductConstants {
   static const imageRatio = 161 / 242;
 
+  static const defaultListItemsCount = 20;
   static const defaultGridItemsCount = 20;
   static const gridItemWidth = 160.0;
   static const gridItemHeight = 240.0;
