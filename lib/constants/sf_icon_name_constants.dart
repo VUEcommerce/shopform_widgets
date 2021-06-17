3baefefa4fed85241ebf92c9ed3a63de06420e23
TungPT95 <tung@shopform.sg>
2021-06-17 07:52:49 +0700

    add asset string

--- lib/constants/sf_icon_name_constants.dart
+++ lib/constants/sf_icon_name_constants.dart
@@ -251,3 +251,4 @@ const squaredTickIcon = '${folderAssetsIcons}squared_tick_icon.svg';
 const productNotFound = '${folderAssetsImages}product_not_found.svg';
 const appIcon = '${folderAssetsLogo}shopform_logo_black.png';
 const reportIllustration = '${folderAssetsIcons}report_illustration.svg';
+const iconHide = '${folderAssetsIcons}icon_hide.svg';
